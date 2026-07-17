d79b79d18fd82ffdb2fb2a8a541e6648febc21b9
SAM-BIM-AI <vm@sambim.xyz>
2026-07-17 13:30:22 +0200

    Update NavigateTo.cs

--- Grasshopper/SAM.Core.Grasshopper/Modify/NavigateTo.cs
+++ Grasshopper/SAM.Core.Grasshopper/Modify/NavigateTo.cs
@@ -6,6 +6,7 @@ using Grasshopper.GUI.Canvas;
 using Grasshopper.Kernel;
 using System;
 using System.Collections.Generic;
+using System.Drawing;
 
 namespace SAM.Core.Grasshopper
 {
@@ -58,18 +59,39 @@ namespace SAM.Core.Grasshopper
                 attributes.Selected = true;
             }
 
-            if (attributesList.Count == 1)
-            {
-                gH_Canvas.Viewport.Focus(attributesList[0]);
-            }
-            else
-            {
-                gH_Canvas.Viewport.Focus(attributesList);
-            }
+            CenterViewport(gH_Canvas, attributesList);
 
             gH_Canvas.Refresh();
 
             return true;
+        }
+
+        private static void CenterViewport(GH_Canvas gH_Canvas, List<IGH_Attributes> attributesList)
+        {
+            RectangleF bounds = attributesList[0].Bounds;
+            for (int i = 1; i < attributesList.Count; i++)
+            {
+                bounds = RectangleF.Union(bounds, attributesList[i].Bounds);
+            }
+
+            bounds.Inflate(50, 50);
+
+            GH_Viewport gH_Viewport = gH_Canvas.Viewport;
+
+            float zoom = gH_Viewport.Zoom;
+            if (bounds.Width > 0 && bounds.Height > 0)
+            {
+                Rectangle screenPort = gH_Viewport.ScreenPort;
+                if (screenPort.Width > 0 && screenPort.Height > 0)
+                {
+                    float fitZoom = Math.Min(screenPort.Width / bounds.Width, screenPort.Height / bounds.Height);
+                    zoom = Math.Min(1.0f, fitZoom);
+                }
+            }
+
+            gH_Viewport.Zoom = zoom;
+            gH_Viewport.MidPoint = new PointF(bounds.X + bounds.Width / 2f, bounds.Y + bounds.Height / 2f);
+            gH_Viewport.ComputeProjection();
         }
     }
 }
